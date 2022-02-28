09bb934e6fa51d376b5fc29dd57adec69797cff7
MoonlightSmile <362652565@qq.com>
2022-02-28 09:18:12 +0000

    Deploying to gh-pages from @ MoonlightSmile/space-shooter.js@6deb565b16a56d305427d02d364c1818a0cbc498 🚀

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { Game } from "phaser";
-
-declare global {
-  interface Window {
-    g_game: Game;
-  }
-}
-
-export {};
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-/**
- * @type {import('vite').UserConfig}
- */
-const config = {
-  // 配置选项
-  base: "/space-shooter.js/dist/",
-};
